e6e197546ef4add92890b7d5e2dadc4317f71cab
Alan Chen <alanc2k17@gmail.com>
2016-12-07 21:03:18 -0500

    needs more salt

--- utils/dbm.py
+++ utils/dbm.py
@@ -4,15 +4,29 @@ db = sqlite3.connect("data/dab.db")
 d = db.cursor()
 
 # =========== START ACCESSOR METHODS =============
-def get_meme(url):
-    q = 'SELECT ref FROM memelist WHERE url=\"%s\";' % (url)
+def get_price(url):
+    q = 'SELECT price FROM memelist WHERE ref=\"%s\";' % (url)
+    d.execute(q)
+    r = d.fetchall()
+
+    return r
+
+def get_owner(url):
+    q = 'SELECT owner FROM memelist WHERE ref=\"%s\";' % (url)
+    d.execute(q)
+    r = d.fetchall()
+
+    return r
+
+def get_amtsold(url):
+    q = 'SELECT amtsold FROM memelist WHERE ref=\"%s\";' % (url)
     d.execute(q)
     r = d.fetchall()
 
     return r
 
 def get_owned(userid):
-    q = 'SELECT memeid FROM memelist WHERE owner=%s' % (userid)
+    q = 'SELECT memeid FROM memelist WHERE owner=%s;' % (userid)
     d.execute(q)
     r = d.fetchall()
 
@@ -25,9 +39,21 @@ def get_topfive():
 
     return r
 
+def get_balance(userid):
+    q = 'SELECT balance FROM userdata WHERE username=\"%s\";' % (userid)
+    d.execute(q)
+    r = d.fetchall()
+
+    return r
+
+def get_lastmemesold(userid):
+    q = 'SELECT lastmemesold FROM userdata WHERE username=\"%s\";' % (userid)
+    d.execute(q)
+    r = d.fetchall()
+
 # =========== END ACCESSOR METHODS =============
 
-# =========== START GET METHODS ================
+# =========== START MUTATOR METHODS ================
 def add_meme(price, owner, url):
     q = 'INSERT INTO memelist(price, owner, amtsold, ref) VALUES (%s, %s, 1, \"%s\");' % (price, owner, url)
     d.execute(q)
@@ -37,7 +63,6 @@ def add_meme(price, owner, url):
 def set_owner(url, nowner):
     q = 'UPDATE memelist SET owner=%s WHERE url=\"%s\";' % (nowner, url)
     d.execute(q)
-
     db.commit()
 
 def set_price(url, nprice):
@@ -51,7 +76,20 @@ def incrmt_amtsold(url):
     d.execute(q)
 
     db.commit()
-# =========== END GET METHODS ================
+
+def set_balance(userid, nbalance):
+    q = 'UPDATE userdata SET balance=%s WHERE username=\"%s\";' % (nbalance, userid)
+    d.execute(q)
+
+    db.commit()
+
+def set_lastmemesold(userid, nmemeid):
+    q = 'UPDATE userdata SET lastmemesold=%s WHERE username=\"%s\";' % (nmemeid, userid)
+    d.execute(q)
+
+    db.commit()
+
+# =========== END MUTATOR METHODS ================
 
 # =========== LOGIN/REGISTER =================
 def auth(user, password):
@@ -86,3 +124,4 @@ def register(user, password):
         d.execute(q)
 
         db.commit()
+# =========== END LOGIN/REGISTER ===================
